3a94f0746c2afbe678d0e22278d74c1f291b12ae
Obada Bitar <c4013337@hallam.shu.ac.uk>
2024-11-24 00:53:21 +0000

    TASK 11 - COMPLETED

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//TASK 8 
 let list = document.querySelectorAll('p .colPicker');
 
 list.forEach(li => {
@@ -15,7 +16,74 @@ list.forEach(li => {
     })
 })
 
-// for(let i = 1; i < li.length; i++)
-// {
-//     li[i].style.backgroundColor = "green";
-// }
+
+//TASK 9
+
+imgSrc = document.querySelector("#myImages")
+
+let imgArrSrc =
+[
+    'images/view1.jpg',
+    'images/view2.jpg',
+    'images/view3.jpg',
+    'images/view4.jpg',
+    'images/view5.jpg',
+    'images/view6.jpg'
+]
+
+
+let i = 0;
+let intrevalId;
+
+function autoImageRotator(){
+    intrevalId = setInterval(() => {
+        if (i + 1 < imgArrSrc.length) {
+            i += 1; 
+        } else {
+            i = 0;
+            console.info("Resetting image source index");
+        }
+        imgSrc.setAttribute('src', imgArrSrc[i]);
+        console.info(i);
+    }, 1000); 
+}
+
+//TASK 11 - MXIING AUTO AND MANUAL IMAGE ROTATOR
+
+let imgItem = document.querySelector("#myImages");
+
+// autoImageRotator()
+
+let mouseOut = false
+
+imgItem.addEventListener('mouseover',() =>{
+    clearInterval(intrevalId);
+    mouseOut = true;
+    imgItem.addEventListener('click', () => {
+        console.info("clicked")
+        if (i + 1 < imgArrSrc.length) {
+            i+=1;
+        }
+        else{
+            i = 0; 
+        };
+        imgSrc.setAttribute('src', imgArrSrc[i])
+    })
+} )
+    
+imgItem.addEventListener('mouseout', () => {
+    if (i + 1 < imgArrSrc.length) {
+        i += 1; 
+    } else {
+        i = 0;
+        console.info("Resetting image source index");
+    }
+    imgSrc.setAttribute('src', imgArrSrc[i])
+    autoImageRotator(); console.info("mouse leave")
+} );
+
+
+
+//TASK 12
+
+console.dir(window.location)
